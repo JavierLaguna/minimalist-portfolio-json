f3bb5245596724a275ee13cb546d7489b5d661c9
Javier Laguna <javier.laguna@innocv.com>
2024-03-01 14:57:20 +0100

    Add Vercel analytics WIP
    # Conflicts:
    #	astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  i18n: { locales: ["en", "es"], defaultLocale: "en" },
+i18n: { locales: ["en", "es"], defaultLocale: "en" },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
