e762273a1b0f36ced6442805aeecce596ccfd83a
Javier Laguna <javier.laguna@innocv.com>
2026-02-26 12:13:39 +0100

    Rebase fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-i18n: { locales: ["en", "es"], defaultLocale: "en" },
-  output: 'server',
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+  output: "server",
   adapter: vercel({
-    webAnalytics: { enabled: true }
+    webAnalytics: { enabled: true },
   }),
 });
